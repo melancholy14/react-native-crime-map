3dc75f8a19796f0fda79217ab11eb2f32358fed6
melancholy14 <melancholy8914@gmail.com>
2019-09-22 17:05:07 +0100

    Add: add search/map/analysis screens

---
+++ screens/AnalysisScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function AnalysisScreen() {
+  return (<View>
+    <Text>AnalysisScreen</Text>
+  </View>);
+}
+
+export default AnalysisScreen;
